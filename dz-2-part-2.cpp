adf25f7776772727b0073393727f1551fd307997
Ирина <99885861+iruuuk@users.noreply.github.com>
2022-03-17 20:40:10 +0300

    Update dz-2-part-2.cpp

--- dz-2-part-2.cpp
+++ dz-2-part-2.cpp
@@ -4,22 +4,22 @@ using namespace std;
 
 int main()
 {
-cout « "ввод числа:\n";
-string s;
-getline(cin, s);
+ cout « "ввод числа:\n";
+ string s;
+ getline(cin, s);
 
-bool ans = true;
-while (s.length() != 0)
+ bool ans = true;
+ while (s.length() != 0)
 {
-int n = stoi(s); // convert string to number (int)
-ans &= n > 0;
-getline(cin, s);
+  int n = stoi(s); // convert string to number (int)
+  ans &= n > 0;
+  getline(cin, s);
 }
 
-if (ans)
-cout « "вывод: все числа положительны\n";
-else
-cout « "вывод: не все числа положительны\n";
+ if (ans)
+ cout « "вывод: все числа положительны\n";
+ else
+ cout « "вывод: не все числа положительны\n";
 
 return 0;
 }
